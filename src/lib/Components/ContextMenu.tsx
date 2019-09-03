c8918b3cddc8eadbd010cb0bb71bca5ac5c4565c
Patryk Eliasz <patryk.eliasz@silevis.com>
2019-09-03 08:21:14 +0200

    rendering dropdown depending on free space

--- src/lib/Components/ContextMenu.tsx
+++ src/lib/Components/ContextMenu.tsx
@@ -26,42 +26,15 @@ const ContextMenuDescription = (props: any) => {
     )
 }
 
-const ContextMenuItem = (props: {level: number, item: MenuOption, state: State}) => {
-    const {level, item, state } = props;
-    return (
-        <div
-            data-level={level}
-            className="dg-context-menu-option"
-            onPointerDown={e => e.stopPropagation()}
-            onClick={() => {
-                item.handler();
-                state.updateState((state: State) => ({ ...state, selectedIds: state.selectedIds, contextMenuPosition: [-1, -1] }))
-            }}> 
-            <ContextMenuDescription title={item.title} hasChilds={item.childs && item.childs.length > 0} iconURL='https://img.icons8.com/color/2x/image.png'/>
-            {item.childs && 
-            <div className="dg-child-submenu-container">
-                {item.childs.map((item, idx) => {
-                    return <ContextMenuItem level={level+1} key={idx} state={state} item={item}/>
-                })}
-            </div>}
-        </div>
-    )
-}
-
-
-export class ContextMenu extends React.Component<ContextMenuProps> {
-
-    renderMenuOption = (level: number, contextMenuOptions: MenuOption[], state: State): any => {
-        return contextMenuOptions.map((item, idx) => {
-            return <ContextMenuItem level={level} key={idx} state={state} item={item}/>
-        })
-    }
+class ContextMenuSubContainer extends React.Component<{ parentLevel: number, level: number, item: MenuOption, state: State, container: DOMRect | ClientRect, contextMenuPosition: number[]  }> {
 
     state = {
         contextMenuDimensions: {
             width: -1,
             height: -1,
-        },
+            left: -1,
+            right: -1,
+        }
     };
 
     componentDidMount() {
@@ -69,6 +42,135 @@ export class ContextMenu extends React.Component<ContextMenuProps> {
             contextMenuDimensions: {
                 width: this.container && this.container.width,
                 height: this.container && this.container.height,
+                left: this.container && this.container.left,
+                right: this.container && this.container.right,
+            }
+        });
+    }
+
+    componentWillUnmount() {
+        this.setState({
+            contextMenuDimensions: {
+                width: -1,
+                height: -1,
+                left: -1,
+                right: -1,
+            }
+        });
+    }
+
+    renderContent() {
+        const { parentLevel, level, item, state, container, contextMenuPosition } = this.props;
+        const { contextMenuDimensions } = this.state;
+
+        // if (contextMenuDimensions && contextMenuDimensions.height) {
+        let pos = [0, 0];
+
+        // pos[0] = container.height;
+        
+        if ( contextMenuDimensions && contextMenuDimensions.width !== -1 && contextMenuDimensions.height !== -1) {
+
+            pos[1] = container.width - contextMenuPosition[0] - contextMenuDimensions.width;
+            console.log(container);
+
+            const vieportWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+            const vieportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+        
+            const leftSpace = contextMenuDimensions.right - container.width + contextMenuDimensions.width;
+            console.warn(`leftSpace: ${leftSpace}}, contextMenuDimensions.right: ${contextMenuDimensions.right} ,contextMenuDimensions.width ${contextMenuDimensions.width}`);
+
+            if (vieportWidth < leftSpace) {
+                console.log('nie mieszcze sie');
+            }
+                
+        }
+
+        return (
+            <div 
+                className="dg-child-submenu-container"
+                // style={{display: parentLevel !== level - 1 ? 'none' : 'block'}}
+                style={{
+                    top: pos[0] + 'px',
+                    right: pos[1] + 'px'
+                }}> 
+                {item && item.childs && item.childs.map((item, idx) => {
+                    return <ContextMenuItem level={level+1} key={idx} state={state} item={item} container={container} contextMenuPosition={pos}/>
+                })}
+            </div>
+        )
+    }
+
+    private container!: DOMRect | ClientRect;
+
+    render() {
+        const { contextMenuDimensions } = this.state;
+        return (
+            <div 
+                ref={(el: HTMLDivElement) => {
+                    this.container = el && el.children[0] && el.children[0].getBoundingClientRect()
+                    if (this.container) {
+                        // console.log(`Context: ${el.children[0].getBoundingClientRect()}`);
+                    }
+                }}>
+                {contextMenuDimensions && this.renderContent()}
+            </div>
+        );
+    }
+}
+
+class ContextMenuItem extends React.Component<{level: number, item: MenuOption, state: State, container: DOMRect | ClientRect, contextMenuPosition: number[] }> {
+    state = { hovered: false };
+    render() {
+        const {level, item, state, container, contextMenuPosition } = this.props;
+        return (
+            <div
+                data-level={level}
+                className="dg-context-menu-option"
+                onPointerDown={e => e.stopPropagation()}
+                onMouseEnter={() => this.setState({ hovered: true })}
+                onMouseLeave={() => this.setState({ hovered: false })}
+                onClick={() => {
+                    item.handler();
+                    state.updateState((state: State) => ({ ...state, selectedIds: state.selectedIds, contextMenuPosition: [-1, -1] }))
+                }}> 
+                <ContextMenuDescription title={item.title} hasChilds={item.childs && item.childs.length > 0} iconURL='https://img.icons8.com/color/2x/image.png'/>
+                {item.childs && this.state.hovered &&
+                    <ContextMenuSubContainer parentLevel={level-1} level={level} state={state} item={item} container={container} contextMenuPosition={contextMenuPosition}/>}
+            </div>
+        )
+    }
+}
+
+
+export class ContextMenu extends React.Component<ContextMenuProps> {
+
+    renderMenuOption = (level: number, contextMenuOptions: MenuOption[], state: State, container: DOMRect | ClientRect, contextMenuPosition: number[]): any => {
+        return contextMenuOptions.map((item, idx) => {
+            return <ContextMenuItem level={level} key={idx} state={state} item={item} container={container} contextMenuPosition={contextMenuPosition}/>
+        })
+    }
+
+    state = {
+        contextMenuDimensions: {
+            width: -1,
+            height: -1,
+        }
+    };
+
+    componentDidMount() {
+        this.setState({
+            contextMenuDimensions: {
+                width: this.container && this.container.width,
+                height: this.container && this.container.height,
+            }
+        });
+    }
+
+    componentWillUnmount() {
+        this.setState({
+            contextMenuDimensions: {
+                width: -1,
+                height: -1,
             }
         });
     }
@@ -110,7 +212,7 @@ export class ContextMenu extends React.Component<ContextMenuProps> {
                     top: contextMenuPosition[0] + 'px',
                     left: contextMenuPosition[1] + 'px'
                 }}> 
-                {this.renderMenuOption(0, contextMenuOptions, state)}
+                {this.renderMenuOption(0, contextMenuOptions, state, this.container, contextMenuPosition)}
             </div>
         );
     }
@@ -122,10 +224,7 @@ export class ContextMenu extends React.Component<ContextMenuProps> {
         return (
             <div 
                 ref={(el: HTMLDivElement) => {
-                    this.container = el && el.children[0].getBoundingClientRect(); 
-                    if (el) {
-                        console.log(el.children[0].getBoundingClientRect());
-                    }
+                    this.container = el && el.children[0].getBoundingClientRect()
                 }}>
                 {contextMenuDimensions && this.renderContent()}
             </div>
