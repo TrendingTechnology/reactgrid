b59697e33488e6a06b72bdb5191dbffbd566df83
Patryk Eliasz <patryk0493@gmail.com>
2019-10-16 09:17:01 +0200

    fix #80

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,5 @@
 // This modules are exported by the lib
 export * from './Components/ReactGrid'
 export * from './Common/PublicModel'
+export * from './Common/Constants'
+export * from './CellTemplates/keyCodeCheckings'
