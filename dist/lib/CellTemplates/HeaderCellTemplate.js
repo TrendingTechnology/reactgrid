a4db2cdb20f38827ae5f83f220ae9b92e3efd006
Patryk Eliasz <patryk0493@gmail.com>
2019-10-29 12:11:07 +0100

    fixed checkbox and header cell

--- dist/lib/CellTemplates/HeaderCellTemplate.js
+++ dist/lib/CellTemplates/HeaderCellTemplate.js
@@ -1,11 +1,12 @@
+import * as React from 'react';
 var HeaderCellTemplate = (function () {
     function HeaderCellTemplate() {
         this.isReadonly = function () { return true; };
         this.isValid = function (cellData) { return (typeof (cellData) === 'string'); };
         this.isFocusable = function () { return false; };
         this.cellDataToText = function (cellData) { return cellData; };
-        this.getCustomStyle = function (cellData) { return ({ background: '#f3f3f3' }); };
-        this.renderContent = function (props) { return props.cellData; };
+        this.getCustomStyle = function (cellData) { return ({ background: 'rgba(0, 0, 0, 0.07)' }); };
+        this.renderContent = function (props) { return React.createElement("div", { className: "rg-header-cell" }, props.cellData); };
     }
     return HeaderCellTemplate;
 }());
