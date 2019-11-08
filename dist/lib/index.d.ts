16924614e3ccf4b02ea3e70952aa13394e8087b8
miki10194 <miki10194@wp.pl>
2019-11-08 14:05:06 +0100

    update build

--- dist/lib/index.d.ts
+++ dist/lib/index.d.ts
@@ -1,3 +1,4 @@
 export * from './Components/ReactGrid';
 export * from './Model/PublicModel';
 export * from './CellTemplates/keyCodeCheckings';
+export * from './Functions/keyCodes';
--- dist/lib/index.js
+++ dist/lib/index.js
@@ -1,2 +1,3 @@
 export * from './Components/ReactGrid';
 export * from './CellTemplates/keyCodeCheckings';
+export * from './Functions/keyCodes';
