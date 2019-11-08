9fdb01d8a66f85aadcd3a2d4a4fda31b484a8760
miki10194 <miki10194@wp.pl>
2019-11-08 12:25:34 +0100

    added preventDefault in handleKeyDown

--- src/lib/Functions/handleKeyDown.ts
+++ src/lib/Functions/handleKeyDown.ts
@@ -6,7 +6,7 @@ import { getCompatibleCellAndTemplate } from './getCompatibleCellAndTemplate';
 
 export function handleKeyDown(state: State, event: KeyboardEvent): State {
     const newState = handleKeyDownInternal(state, event);
-    if (newState !== state) { event.stopPropagation(); }
+    if (newState !== state) { event.stopPropagation(); event.preventDefault(); }
     return newState;
 }
 
