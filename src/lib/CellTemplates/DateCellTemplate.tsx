0509beb71a82119dddca4f6cc436ea3dca4b042f
Patryk Eliasz <patryk0493@gmail.com>
2019-11-20 10:33:45 +0100

    cleaned date cell

--- src/lib/CellTemplates/DateCellTemplate.tsx
+++ src/lib/CellTemplates/DateCellTemplate.tsx
@@ -3,7 +3,7 @@ import { keyCodes } from '../Functions/keyCodes';
 import { CellTemplate, Cell, Compatible, Uncertain, UncertainCompatible } from '../Model';
 import { inNumericKey, isNavigationKey, isAlphaNumericKey } from './keyCodeCheckings'
 import { getCellProperty } from '../Functions/getCellProperty';
-import { type } from 'os';
+import { parse } from 'path';
 
 export interface DateCell extends Cell {
     type: 'date';
@@ -16,7 +16,9 @@ export class DateCellTemplate implements CellTemplate<DateCell> {
     getCompatibleCell(uncertainCell: Uncertain<DateCell>): Compatible<DateCell> {
         const date = getCellProperty(uncertainCell, 'date', 'object');
         const dateFormat = uncertainCell.format || new Intl.DateTimeFormat(window.navigator.language);
-        return { ...uncertainCell, date, value: date.getTime(), text: dateFormat.format(date) }
+        const value = date.getTime();
+        const text = dateFormat.format(date);
+        return { ...uncertainCell, date, value, text }
     }
 
     handleKeyDown(cell: Compatible<DateCell>, keyCode: number, ctrl: boolean, shift: boolean, alt: boolean): { cell: Compatible<DateCell>, enableEditMode: boolean } {
@@ -32,28 +34,23 @@ export class DateCellTemplate implements CellTemplate<DateCell> {
 
     render(cell: Compatible<DateCell>, isInEditMode: boolean, onCellChanged: (cell: Compatible<DateCell>, commit: boolean) => void): React.ReactNode {
 
-        if (!isInEditMode) return cell.text;
+        if (!isInEditMode) 
+            return cell.text;
 
-        const locale = cell.format ? cell.format.resolvedOptions().locale : window.navigator.languages[0];
-        const dateParts = new Intl.DateTimeFormat(locale, { year: 'numeric', month: '2-digit', day: '2-digit' }).formatToParts(cell.date);
-
-        const yearPart = dateParts.find(obj => obj.type === 'year');
-        const monthPart = dateParts.find(obj => obj.type === 'month');
-        const dayPart = dateParts.find(obj => obj.type === 'day');
-
-        const year = yearPart ? yearPart.value : 0;
-        const month = monthPart ? monthPart.value : 0;
-        const day = dayPart ? dayPart.value : 0;
-
+        const defaultDate = `${cell.date.getFullYear()}-${(cell.date.getMonth() + 1)}-${cell.date.getDate()}`;
+        
         return <input
             ref={input => {
                 if (input) input.focus();
             }}
             type="date"
-            defaultValue={`${year}-${month}-${day}`}
+            defaultValue={defaultDate}
             onChange={e => {
-                const date = new Date(Date.parse(e.currentTarget.value));
-                onCellChanged(this.getCompatibleCell({ ...cell, date}), false)
+                const timestamp = Date.parse(e.currentTarget.value);
+                if (!Number.isNaN(timestamp)) {
+                    const date = new Date(timestamp);
+                    onCellChanged(this.getCompatibleCell({ ...cell, date }), false)
+                }
             }}
             onKeyDown={e => {
                 if (inNumericKey(e.keyCode) || isNavigationKey(e.keyCode) || (e.keyCode === keyCodes.COMMA || e.keyCode === keyCodes.PERIOD)) e.stopPropagation();
