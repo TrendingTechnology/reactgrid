66083004e9ba6b0645d8c18d631cd0ae01382b01
Patryk Eliasz <patryk0493@gmail.com>
2019-12-02 08:16:45 +0100

    switched to default test grid

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { TestGrid } from "./TestGrid"
-import { GroupTestGrid } from './GroupTestGrid'
+// import { GroupTestGrid } from './GroupTestGrid'
 // DO NOT MOVE
 // this index.tsx is required by react-scripts-ts
 ReactDOM.render(
-  // <TestGrid />,
-  <GroupTestGrid />,
+  <TestGrid />,
+  // <GroupTestGrid />,
   document.getElementById('root') as HTMLElement
 );
