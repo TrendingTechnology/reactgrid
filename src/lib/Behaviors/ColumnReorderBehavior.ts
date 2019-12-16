74717bc174bacf32f4b602d555667304b5aefa7f
myskamil <kamil.myslek@silevis.com>
2019-12-16 09:44:36 +0100

    fix line position - not finished

--- src/lib/Behaviors/ColumnReorderBehavior.ts
+++ src/lib/Behaviors/ColumnReorderBehavior.ts
@@ -1,10 +1,12 @@
-import { State, Behavior, PointerEvent, PointerLocation, Direction } from '../Model';
+import { State, Behavior, PointerEvent, PointerLocation, Direction, GridColumn } from '../Model';
 
 export class ColumnReorderBehavior extends Behavior {
     private initialColumnIdx!: number;
     private lastPossibleDropLocation?: PointerLocation;
     private pointerOffset!: number;
     private selectedIdxs!: number[];
+    private rightOffset!: number;
+    private leftOffset!: number;
     autoScrollDirection: Direction = 'horizontal';
 
     handlePointerDown(event: PointerEvent, location: PointerLocation, state: State): State {
@@ -16,6 +18,8 @@ export class ColumnReorderBehavior extends Behavior {
         const leftColumns = leftIndexes.map(i => state.cellMatrix.columns[i]);
         const leftColumnsWidth = leftColumns.reduce((sum, col) => sum + col.width!, 0);
         this.pointerOffset = leftColumnsWidth + location.cellX;
+        this.leftOffset = location.viewportX - this.getLeftXViewport(state);
+        this.rightOffset = this.getRightXViewport(state) - location.viewportX;
         return {
             ...state,
             lineOrientation: 'vertical',
@@ -42,11 +46,32 @@ export class ColumnReorderBehavior extends Behavior {
         return x;
     }
 
+    getLeftXViewport(state: State): number {
+        const leftColumns = this.selectedIdxs.map(i => state.cellMatrix.columns[i].left);
+        return Math.min(...leftColumns);
+    }
+
+    getRightXViewport(state: State): number {
+        const rightColumns = this.selectedIdxs.map(i => state.cellMatrix.columns[i].right);
+        return Math.max(...rightColumns);
+    }
+
+    getXPositionForSelection(state: State, edge: number): number {
+        const column = state.cellMatrix.columns.find((gridColumn: GridColumn) => {
+            return gridColumn.left <= edge && gridColumn.right >= edge;
+        });
+        if (column === undefined) {
+            return 0;
+        }
+        const centerLine = column.left + (column.width) / 2;
+        return centerLine >= edge ? column.left : column.right;
+    }
+
     handlePointerEnter(event: PointerEvent, location: PointerLocation, state: State): State {
         const dropLocation = this.getLastPossibleDropLocation(location, state)
         if (!dropLocation) return state;
         const drawRight = dropLocation.column.idx > this.initialColumnIdx;
-        const linePosition = Math.min(dropLocation.viewportX - dropLocation.cellX + (drawRight ? dropLocation.column.width : 0) + state.viewportElement.scrollLeft,
+        const linePosition = Math.min((drawRight ? this.getXPositionForSelection(state, dropLocation.viewportX + this.rightOffset) : this.getXPositionForSelection(state, dropLocation.viewportX - this.leftOffset)) + state.viewportElement.scrollLeft,
             state.visibleRange.width + state.cellMatrix.frozenLeftRange.width + state.cellMatrix.frozenRightRange.width + state.viewportElement.scrollLeft
         )
         this.lastPossibleDropLocation = dropLocation;
