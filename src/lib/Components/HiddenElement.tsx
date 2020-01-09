2e4568ea76e3f6665f645758c4a4660b2962fe54
Silevis Mac Mini <silevismacmini@Mac-mini-Silevis.local>
2020-01-09 12:36:17 +0100

    fixed hidden focus element position

--- src/lib/Components/HiddenElement.tsx
+++ src/lib/Components/HiddenElement.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { State } from '../Model';
-import { isBrowserSafari } from '../Functions/safari';
 
 interface HiddenElementProps {
     state: State;
@@ -9,15 +8,5 @@ interface HiddenElementProps {
 
 // TODO apply similar component on LegacyBrowserGridRenderer
 export const HiddenElement: React.FunctionComponent<HiddenElementProps> = (props) => {
-
-    let styles = {};
-    if (isBrowserSafari()) {
-        const { focusedLocation, cellMatrix } = props.state;
-        styles = {
-            position: 'absolute',
-            ...(focusedLocation && { top: focusedLocation.row.top }),
-            ...(focusedLocation && { left: cellMatrix.last.column.right })
-        }
-    }
-    return <input className="rg-hidden-element" style={styles} readOnly={true} ref={props.hiddenElementRefHandler} />
+    return <input className="rg-hidden-element" readOnly={true} ref={props.hiddenElementRefHandler} />
 }
